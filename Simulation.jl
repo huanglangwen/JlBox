9e1bab6f5cc488b682ec0efb55262ac8fd69e9a0
Langwen Huang <huanglangwen10@gmail.com>
2019-02-10 16:22:37 +0000

    add CSV output

--- Simulation.jl
+++ Simulation.jl
@@ -1,6 +1,7 @@
 include("JlBoxModule.jl")
 using Compute:run_simulation
 using DataFrames
+using CSV
 
 #Profile.init(n = 10^7, delay = 5.)
 @time sol,reactants2ind=run_simulation()
@@ -9,7 +10,7 @@ ind2reactants=Dict(reactants2ind[key]=>key for key in keys(reactants2ind))
 reactants=[ind2reactants[ind] for ind in 1:num_reactants]
 df=DataFrame(transpose(sol))
 names!(df,[Symbol(reac) for reac in reactants])
-#writetable("results.csv",df)
+CSV.write("results.csv",df)
 df
 #@profile run_simulation()
 #open("prof.txt", "w") do s
