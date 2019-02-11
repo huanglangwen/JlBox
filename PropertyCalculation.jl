986524491354410ebfbab5a37794c7f534d7e750
Langwen Huang <huanglangwen10@gmail.com>
2019-02-11 23:03:19 +0000

    complete compoundProperty()

--- PropertyCalculation.jl
+++ PropertyCalculation.jl
@@ -34,6 +34,48 @@ function SMILES2Pybel(smi_str)
     return pybel.readstring("smi",smi_str)
 end
 
-function compoudProperty(compound_str)
-    nothing
+function compoudProperty(compound_str,temperature,methodfuncs,species2SMILESdict)
+    boiling_point,vapour_pressure,critical_property,liquid_density=[methodfuncs[i] for i in ["bp","vp","critical","density"]]
+    pybelobj=SMILES2Pybel(species2SMILESdict[compound_str])
+    b1=boiling_points.nannoolal(pybelobj)
+    density=liquid_density(pybelobj, temperature, critical_property(pybelobj, b1))*1.0E3
+    mw=pybelobj[:molwt]
+    groups_dict=groups.composition(pybelobj)
+    o_c=groups_dict["O"]/groups_dict["C"]
+    h_c=groups_dict["H"]/groups_dict["C"]
+    b=boiling_point(pybelobj)
+    sat_vp=vapour_pressure(pybelobj, temperature,b)
+    return density,mw,o_c,h_c,sat_vp
+end
+
+function Pure_component1(num_species::Integer,species_names::Array{String,1},temperature::Number,methods::Dict)
+    species2inds=Dict(species_names[i]=>i for i in 1:num_species)
+    bp_method,vp_method,critical_method,density_method=[methods[i] for i in ["bp","vp","critical","density"]]
+    boiling_point = Dict(
+        "joback_and_reid"=> boiling_points.joback_and_reid,
+        "stein_and_brown"=> boiling_points.stein_and_brown,
+        "nannoolal"=>       boiling_points.nannoolal,
+    )[bp_method]
+    vapour_pressure = Dict(
+        "nannoolal"=>            vapour_pressures.nannoolal,
+        "myrdal_and_yalkowsky"=> vapour_pressures.myrdal_and_yalkowsky,
+        # Evaporation doesn"t use boiling point
+        "evaporation"=> (c, t, b)-> vapour_pressures.evaporation(c, t),
+    )[vp_method]
+    critical_property = Dict(
+        "nannoolal"=>          critical_properties.nannoolal,
+        "joback_and_reid"=>    critical_properties.joback_and_reid,
+    )[critical_method]
+    liquid_density = Dict(
+        "girolami"=>      (c, t, p) -> liquid_densities.girolami(c),
+        "schroeder"=>     liquid_densities.schroeder,
+        "le_bas"=>        liquid_densities.le_bas,
+        "tyn_and_calus"=> liquid_densities.tyn_and_calus,
+    )[density_method]
+    methodfuncs=Dict(
+        "bp"=>boiling_point,
+        "vp"=>vapour_pressure,
+        "critical"=>critical_property,
+        "density"=>liquid_density
+    )
 end
