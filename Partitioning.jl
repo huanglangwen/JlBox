37eeb7f94907513603f0c1ccf55b47499b78b704
Langwen Huang <huanglangwen10@gmail.com>
2019-02-18 01:03:41 +0000

    minor correction

--- Partitioning.jl
+++ Partitioning.jl
@@ -1,5 +1,5 @@
 function Partition!(y,dy_dt,dy_dt_gas_matrix,C_g_i_t,
-                    num_bins,num_reactants,num_reactants_condensed,include_inds
+                    num_bins,num_reactants,num_reactants_condensed,include_inds,
                     mw_array,density_array,gamma_gas,alpha_d_org,DStar_org,Psat,N_perbin,
                     NA,sigma,R_gas,Model_temp)
     size_array=zeros(Float64,num_bins)
