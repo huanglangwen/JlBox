5a720ffdfbf056b3e21d92570759cfd7998984f7
hlw <huanglangwen@icloud.com>
2019-02-18 12:46:01 +0000

    finish lognormal distribution

---
+++ Size_Distributions.jl
@@ -0,0 +1,10 @@
+
+function lognormal(num_bins::Integer,total_conc::Real,meansize::Real,
+                   std::Real,lowersize::Real,uppersize::Real)
+    mu=log(meansize)
+    sigma=log(std)
+    x_output=exp.(linspace(log(lowersize),log(uppersize),num_bins))
+    pdf=x->exp(-(log(x)-mu)^2/(2*sigma^2))/(x*sigma*sqrt(2*pi))
+    pdf_output=[pdf(x) for x in x_output]
+    return (pdf_output./sum(pdf_output))*total_conc,x_output
+end
