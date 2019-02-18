d9a143cb8231478d1a5b67a396c847f8b8b8d7bf
Langwen Huang <huanglangwen@outlook.com>
2019-02-18 13:03:47 +0000

    delete water config

--- Configure_aerosol.jl
+++ Configure_aerosol.jl
@@ -8,12 +8,12 @@ const batch_step=100.0 # seconds
 const temp_celsius=temp-273.15
 #const Psat=610.78*exp((temp_celsius/(temp_celsius+238.3))*17.2694)# Saturation VP of water vapour, to get concentration of H20
 #const Pw=RH*Psat
-const Wconc=0.002166*(Pw/(temp_celsius+273.16))*1.0e-6 #kg/cm3
-const H2O=Wconc*(1.0/(18.0e-3))*6.0221409e+23#Convert from kg to molecules/cc
+#const Wconc=0.002166*(Pw/(temp_celsius+273.16))*1.0e-6 #kg/cm3
+#const H2O=Wconc*(1.0/(18.0e-3))*6.0221409e+23#Convert from kg to molecules/cc
 const tspan=(0,simulation_time)
 const Cfactor= 2.55e+10 #ppb-to-molecules/cc
 const reactants_initial_dict=Dict(["O3"=>18.0,"APINENE"=>30.0])#ppm 
-const constantdict=Dict([(:temp,temp),(:H2O,H2O)])
+const constantdict=Dict([(:temp,temp)])
 
 const num_bins=16
 #Lognormal Distribution
