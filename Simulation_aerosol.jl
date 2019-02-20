db870342c51bb71707e78348f1e68a007c0b9c72
hlw <huanglangwen@icloud.com>
2019-02-20 10:47:57 +0000

    minor correction

--- Simulation_aerosol.jl
+++ Simulation_aerosol.jl
@@ -10,7 +10,10 @@ ind2reactants=Dict(reactants2ind[key]=>key for key in keys(reactants2ind))
 reactants=[Symbol(ind2reactants[ind]) for ind in 1:num_reactants]
 sol_mtx=transpose(sol)
 aerosol_mtx=sol_mtx[1:end,num_reactants+1:num_reactants+num_bins*num_reactants_condensed]
-SOA_array=[sum(sum(reshape(aerosol_mtx[i,1:end],(num_reactants_condensed,num_bins)),2)[1:end-1]) for i in size(aerosol_mtx)[1]]
+t_length=size(aerosol_mtx)[1]
+SOA_array=[sum(sum(reshape(aerosol_mtx[i,1:end],(num_reactants_condensed,num_bins)),2)[1:end-1]) for i in 1:t_length]
+t_index=linspace(0,3600,t_length)
+df_SOA=DataFrame(Time=t_index,SOA_array)
 df=DataFrame(sol_mtx[1:end,1:num_reactants])
 names!(df,reactants)
 CSV.write("/data/jlbox_results.csv",df)
