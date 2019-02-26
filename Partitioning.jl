f6ed2ed6315ce46190ca23ed7dd7c196d03259dd
hlw <huanglangwen@icloud.com>
2019-02-26 16:23:55 +0000

    back to dydt gas matrix since there is no significant performance gain

--- Partitioning.jl
+++ Partitioning.jl
@@ -7,7 +7,7 @@ function Partition!(y::Array{Float64,1},dy_dt::Array{Float64,1},dy_dt_gas_matrix
     total_SOA_mass_array=zeros(Float64,num_bins)
     mass_array=zeros(Float64,num_reactants_condensed+1)
     density_array=zeros(Float64,num_reactants_condensed+1)
-    dy_dt_gas_matrix_sum=zeros(Float64,num_reactants)
+    #dy_dt_gas_matrix_sum=zeros(Float64,num_reactants)
     for size_step=1:num_bins
         start_ind=num_reactants+1+((size_step-1)*num_reactants_condensed)
         stop_ind=num_reactants+(size_step*num_reactants_condensed)
@@ -53,13 +53,13 @@ function Partition!(y::Array{Float64,1},dy_dt::Array{Float64,1},dy_dt_gas_matrix
 
         #ASSIGN dy_dt_gas_matrix
         for ind=1:length(include_inds)
-            dy_dt_gas_matrix_sum[include_inds[ind]]+=dm_dt[ind]
-        #   dy_dt_gas_matrix[include_inds[ind],size_step]=dm_dt[ind]
+            #dy_dt_gas_matrix_sum[include_inds[ind]]+=dm_dt[ind]
+            dy_dt_gas_matrix[include_inds[ind],size_step]=dm_dt[ind]
         end
 
         dy_dt[start_ind:stop_ind]=dm_dt
     end
-    dy_dt[1:num_reactants]=dy_dt[1:num_reactants].-dy_dt_gas_matrix_sum#sum(dy_dt_gas_matrix,dims=2)
+    dy_dt[1:num_reactants]=dy_dt[1:num_reactants].-sum(dy_dt_gas_matrix,dims=2)#dy_dt_gas_matrix_sum
     total_SOA_mass=sum(total_SOA_mass_array)*1.0E12
     return dy_dt,total_SOA_mass
 end
