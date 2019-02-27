b19bc9d6c97cc9c4e1c9de7f38360eb129eb3431
hlw <huanglangwen@icloud.com>
2019-02-27 17:08:54 +0000

    minor corrections

--- Simulation_aerosol_jac.jl
+++ Simulation_aerosol_jac.jl
@@ -7,7 +7,7 @@ using Profile
 
 Profile.clear()
 Profile.init(n = 10^7, delay = 0.01)
-sol_mtx,reactants2ind,SOA_array,num_reactants=run_simulation_aerosol(use_jacobian=true)
+@time sol_mtx,reactants2ind,SOA_array,num_reactants=run_simulation_aerosol(use_jacobian=true)
 open("/data/prof.txt", "w") do s
     Profile.print(IOContext(s, :displaysize => (1000, 500)),format=:flat)
 end
