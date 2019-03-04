029993667283740b841f6ca8ee5b2391a4526375
Langwen Huang <huanglangwen10@gmail.com>
2019-03-04 11:40:43 +0000

    minor correction

--- Sensitivity.jl
+++ Sensitivity.jl
@@ -7,7 +7,7 @@ function SOA_mass_jac(dSOA_dy,y,mw_array,NA,num_reactants,num_reactants_condense
     for bin_ind in 1:num_bins
         start_ind=num_reactants+1+(bin_ind-1)*num_reactants_condensed
         stop_ind=num_reactants+bin_ind*num_reactants_condensed-1#exclude water in the end
-        dSOA_dy[start_ind:stop_ind]=((1.0).*mw_array./NA)[1:num_reactants_condensed-1]
+        dSOA_dy[1,start_ind:stop_ind]=((1.0).*mw_array./NA)[1:num_reactants_condensed-1]
     end
     nothing
 end
