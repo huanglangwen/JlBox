32534b68905d09355c59b61c456e921a4bb2c8a4
hlw <huanglangwen@icloud.com>
2019-03-05 15:08:24 +0000

    minor correction

--- Compute.jl
+++ Compute.jl
@@ -7,7 +7,7 @@ using ..Partitioning:Partition!
 using ..Jacobian:gas_jac!,Partition_jac!
 using ..Sensitivity:SOA_mass_jac!,loss_gain_drate_values!
 using DifferentialEquations
-using DifferentialEquations:CVODE_BDF
+using DifferentialEquations:CVODE_BDF,CVODE_Adams
 using StaticArrays
 using SparseArrays
 using Printf
