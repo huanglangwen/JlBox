c020003c0fe57d1f1d046335794cb0796cc92b3d
hlw <huanglangwen@icloud.com>
2019-03-06 11:01:05 +0000

    switch to adjoint

--- Simulation_aerosol_sensitivity.jl
+++ Simulation_aerosol_sensitivity.jl
@@ -4,7 +4,7 @@ using .Compute:run_simulation_aerosol_DDM,run_simulation_aerosol_adjoint
 using DataFrames
 using CSV
 
-@time dSOA_mass_drate=run_simulation_aerosol_DDM(linsolver=:Dense)
+@time dSOA_mass_drate=run_simulation_aerosol_adjoint(linsolver=:Dense)
 df=DataFrame(dSOA_mass_drate)
 CSV.write("/data/jlbox_sensitivity_dSOAdrate_results.csv",df)
 df
