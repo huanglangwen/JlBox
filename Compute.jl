d91300e3d27cba156c8d78761525f114bc0a77c6
hlw <huanglangwen@icloud.com>
2019-03-06 09:02:35 +0000

    minor correction

--- Compute.jl
+++ Compute.jl
@@ -350,7 +350,7 @@ function run_simulation_aerosol_adjoint(;linsolver::Symbol=:Dense)
     end 
     dSOA_mass_drate[1:num_eqns,1]=quadgk(dgpdt,tstops[1],tstops[2])[1]#quadgk->(val,err) ignore error value
     for i in 2:num_tstops-1
-        dSOA_mass_drate[1:num_eqns,i]=dSOA_mass_drate[1:num_eqns,i-1]+reshape(quadgk(dgdt,tstops[i],tstops[i+1])[1],(num_eqns,1))
+        dSOA_mass_drate[1:num_eqns,i]=dSOA_mass_drate[1:num_eqns,i-1]+reshape(quadgk(dgpdt,tstops[i],tstops[i+1])[1],(num_eqns,1))
     end
     return dSOA_mass_drate
 end
