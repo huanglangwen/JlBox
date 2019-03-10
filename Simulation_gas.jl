9713473b0243cfa87a427eade8bcc5a1b5064c3a
Langwen Huang <huanglangwen10@gmail.com>
2019-03-10 20:25:19 +0000

    update error bound

--- Simulation_gas.jl
+++ Simulation_gas.jl
@@ -6,8 +6,8 @@ using DifferentialEquations:CVODE_BDF
 using OrdinaryDiffEq
 #to = TimerOutput()
 prob=gen_simulation_gas()
-reset_timer!(to); @timeit to "CVODE_BDF Default" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to);println()
-reset_timer!(to); @timeit to "CVODE_BDF Default2" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to);println()
+reset_timer!(to); @timeit to "CVODE_BDF Default" begin sol=solve(prob,CVODE_BDF(),dense=false,abstol=1e-8, reltol=1e-8); end; show(to);println()
+reset_timer!(to); @timeit to "CVODE_BDF Default2" begin sol=solve(prob,CVODE_BDF(),dense=false,abstol=1e-8, reltol=1e-8); end; show(to);println()
 reset_timer!(to); @timeit to "CVODE_BDF GMRES" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to);println()
 reset_timer!(to); @timeit to "CVODE_BDF GMRES2" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to);println()
 reset_timer!(to); @timeit to "KenCarp4" begin sol=solve(prob,KenCarp4(),dense=false); end; show(to);println()
