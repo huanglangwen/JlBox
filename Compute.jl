cbe37dc63a80eb45962ccc28a8f345edc5480a14
hlw <huanglangwen@icloud.com>
2019-03-10 14:47:40 +0000

    finish changing

--- Compute.jl
+++ Compute.jl
@@ -1,4 +1,4 @@
-using ..Parse_eqn:parse_reactants,gen_evaluate_rates
+using ..Parse_eqn:parse_reactants,parse_init
 using ..Optimize:constant_folding!,extract_constants!,mk_reactants_list
 using ..Jacobian:gas_jac!
 using DifferentialEquations
@@ -39,48 +39,32 @@ function loss_gain!(num_reactants::Int,num_eqns::Int,
 end
 
 function dydt!(dydt,reactants::Array{Float64,1},p::Dict,t::Real)
-    rate_values,J,stoich_mtx,stoich_list,reactants_list,RO2_inds,num_eqns,num_reactants=
+    rate_values,stoich_mtx,stoich_list,reactants_list,num_eqns,num_reactants=
         [p[ind] for ind in 
-            ["rate_values","J","stoich_mtx","stoich_list","reactants_list","RO2_inds",
+            ["rate_values","stoich_mtx","stoich_list","reactants_list",
              "num_eqns","num_reactants"]
         ]
-    time_of_day_seconds=start_time+t
-    RO2=sum(reactants[RO2_inds])
-    Base.invokelatest(evaluate_rates!,time_of_day_seconds,RO2,H2O,temp,rate_values,J)# =>ratevalues
     loss_gain!(num_reactants,num_eqns,reactants,stoich_mtx,stoich_list,reactants_list,rate_values,dydt)
-    if p["Simulation_type"]=="gas"
-        p["Current_iter"]+=1
-        citer=p["Current_iter"]
-        if citer%(p["ShowIterPeriod"])==0
-            @printf("Current Iteration: %d, time_step: %e\n",citer,t)
-        end
+    p["Current_iter"]+=1
+    citer=p["Current_iter"]
+    if citer%(p["ShowIterPeriod"])==0
+        @printf("Current Iteration: %d, time_step: %e\n",citer,t)
     end
-    nothing#return dydt
+    nothing
 end
 
 function prepare_gas()
     println("Parsing Reactants")
-    stoich_mtx,reactants_mtx,RO2_inds,num_eqns,num_reactants,reactants2ind=parse_reactants(file)
+    rate_values,stoich_mtx,reactants_mtx,num_eqns,num_reactants,reactants2ind=parse_reactants(mechfile)
+    reactants_init=parse_init(initfile,reactants2ind,num_reactants)
     reactants_list=mk_reactants_list(num_reactants,num_eqns,reactants_mtx)
     stoich_list=mk_reactants_list(num_reactants,num_eqns,stoich_mtx)
     @printf("num_eqns: %d, num_reactants: %d\n",num_eqns,num_reactants)
 
-    println("Generating evaluate_rates()")
-    evaluate_rates_expr=gen_evaluate_rates(file)
-    println("Done Generation")
-    rate_values=zeros(Float64,num_eqns)
-    rate_prods=zeros(Float64,num_eqns)
-    J=zeros(Float64,62)
-    #dydt=zeros(Float64,num_reactants)
-    println("Performing constant folding")
-    constant_folding!(evaluate_rates_expr,constantdict,rate_values);
-    extract_constants!(evaluate_rates_expr);
-    println("Evaluating evaluate_rates&loss_gain codes")
-    #eval(evaluate_rates_expr)
-    param_dict=Dict("rate_values"=>rate_values,"J"=>J,"stoich_mtx"=>stoich_mtx,#"dydt"=>dydt,
-                    "stoich_list"=>stoich_list,"reactants_list"=>reactants_list,"RO2_inds"=>RO2_inds,
+    param_dict=Dict("rate_values"=>rate_values,"stoich_mtx"=>stoich_mtx,#"dydt"=>dydt,
+                    "stoich_list"=>stoich_list,"reactants_list"=>reactants_list,
                     "num_eqns"=>num_eqns,"num_reactants"=>num_reactants)
-    return param_dict,reactants2ind,evaluate_rates_expr
+    return reactants_init,param_dict,reactants2ind
 end
 
 function read_configure!(filename::String)
@@ -98,27 +82,15 @@ end
 
 function run_simulation_gas()
     read_configure!("Configure_gas.jl")
-    param_dict,reactants2ind,evaluate_rates_expr=prepare_gas()
-    eval(evaluate_rates_expr)
+    reactants_init,param_dict,reactants2ind=prepare_gas()
     num_reactants=param_dict["num_reactants"]
-    reactants_initial=zeros(Float64,num_reactants)
-    for (k,v) in reactants_initial_dict
-        reactants_initial[reactants2ind[k]]=v*Cfactor#pbb to molcules/cc
-    end
     println("Solving ODE")
     param_dict["Current_iter"]=0
     param_dict["ShowIterPeriod"]=100
-    param_dict["Simulation_type"]="gas"
     odefun=ODEFunction(dydt!; jac=gas_jac!)
-    prob = ODEProblem{true}(odefun,reactants_initial,tspan,param_dict)
-    sol = solve(prob,CVODE_BDF(linear_solver=:Dense),reltol=1e-6,abstol=1.0e-3,
-                tstops=0:batch_step:simulation_time,saveat=batch_step,# save_everystep=true,
-                dt=1.0e-6, #Initial step-size
-                dtmax=100.0,
-                max_order = 5,
-                max_convergence_failures = 1000,
-                #callback=PositiveDomain(reactants_initial,abstol=1.0e-3)
+    prob = ODEProblem{true}(odefun,reactants_init,tspan,param_dict)
+    sol = solve(prob,CVODE_BDF(linear_solver=:Dense),reltol=1e-8,abstol=1.0e-8,dense=false)
+                #callback=PositiveDomain(reactants_init,abstol=1.0e-3)
                 #isoutofdomain=(u,p,t) -> any(x -> x < 0, u)
-                )
     return sol,reactants2ind
 end
--- JlBoxModule.jl
+++ JlBoxModule.jl
@@ -1,10 +1,10 @@
 module Optimize
-export constant_folding!,extract_constants!,generate_loss_gain,mk_stoich_list
+export mk_stoich_list
 include("Optimize.jl")
 end
 
 module Parse_eqn
-export parse_reactants,gen_evaluate_rates
+export parse_reactants
 include("Parse_eqn.jl")
 end
 
