d95b1d65efa5538c7bbbd932a9f6193e4539c69b
hlw <huanglangwen@icloud.com>
2019-03-10 16:07:11 +0000

    run CVODE twice

--- Simulation_gas.jl
+++ Simulation_gas.jl
@@ -6,6 +6,8 @@ using DifferentialEquations:CVODE_BDF
 using OrdinaryDiffEq
 #to = TimerOutput()
 prob=gen_simulation_gas()
-reset_timer!(to); @timeit to "CVODE_BDF Default" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to)
-reset_timer!(to); @timeit to "CVODE_BDF GMRES" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to)
-reset_timer!(to); @timeit to "KenCarp4" begin sol=solve(prob,KenCarp4(),dense=false); end; show(to)
+reset_timer!(to); @timeit to "CVODE_BDF Default" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to);println()
+reset_timer!(to); @timeit to "CVODE_BDF Default2" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to);println()
+reset_timer!(to); @timeit to "CVODE_BDF GMRES" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to);println()
+reset_timer!(to); @timeit to "CVODE_BDF GMRES2" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to);println()
+reset_timer!(to); @timeit to "KenCarp4" begin sol=solve(prob,KenCarp4(),dense=false); end; show(to);println()
