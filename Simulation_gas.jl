ccd1f78c3922a1509a3d0165a8d040c6bb40263e
hlw <huanglangwen@icloud.com>
2019-03-10 16:05:44 +0000

    minor changes

--- Simulation_gas.jl
+++ Simulation_gas.jl
@@ -1,10 +1,10 @@
 include("JlBoxModule.jl")
-using .Compute:gen_simulation_gas
+using .Compute:gen_simulation_gas,to
 using TimerOutputs
 using DifferentialEquations
 using DifferentialEquations:CVODE_BDF
 using OrdinaryDiffEq
-to = TimerOutput()
+#to = TimerOutput()
 prob=gen_simulation_gas()
 reset_timer!(to); @timeit to "CVODE_BDF Default" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to)
 reset_timer!(to); @timeit to "CVODE_BDF GMRES" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to)
