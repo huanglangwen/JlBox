9f7177cb3e7d0ff17d7ca3fcc7d896b6f2336c4b
hlw <huanglangwen@icloud.com>
2019-03-10 15:25:07 +0000

    rodas4 to ESDIRK

--- Simulation_gas.jl
+++ Simulation_gas.jl
@@ -6,8 +6,6 @@ using DifferentialEquations:CVODE_BDF
 
 const to = TimerOutput()
 prob=gen_simulation_gas()
+reset_timer!(to); @timeit to "ESDIRK" begin sol=solve(prob,ESDIRK54I8L2SA(autodiff=false, diff_type=Val{:forward}, new_jac_conv_bound=Inf), dt=0.16); end; show(to)
 reset_timer!(to); @timeit to "CVODE_BDF Default" begin sol=solve(prob,CVODE_BDF(),dense=false); end; show(to)
 reset_timer!(to); @timeit to "CVODE_BDF GMRES" begin sol=solve(prob,CVODE_BDF(linear_solver=:GMRES),dense=false); end; show(to)
-reset_timer!(to); @timeit to "Rodas4" begin sol=solve(prob,Rodas4(autodiff=false),dense=false,calck=false); end; show(to)
-
-
