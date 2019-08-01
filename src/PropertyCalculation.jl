3e78508e09a57546a8ce6baffee9fd798f75648d
hlw <huanglangwen@icloud.com>
2019-08-01 14:20:33 +0800

    adapt to new pycall

--- src/PropertyCalculation.jl
+++ src/PropertyCalculation.jl
@@ -2,16 +2,16 @@
 using PyCall
 using LightXML
 pushfirst!(PyVector(pyimport("sys")["path"]),"../UManSysProp_public")
-@pyimport umansysprop.boiling_points as boiling_points
-@pyimport umansysprop.vapour_pressures as vapour_pressures
-@pyimport umansysprop.critical_properties as critical_properties
-@pyimport umansysprop.liquid_densities as liquid_densities
-@pyimport umansysprop.partition_models as partition_models
-@pyimport umansysprop.groups as groups
-@pyimport umansysprop.activity_coefficient_models_dev as aiomfac
-@pyimport umansysprop.forms as forms #need forms.CoreAbundanceField (class)
+boiling_points=pyimport("umansysprop.boiling_points")
+vapour_pressures=pyimport("umansysprop.vapour_pressures")
+critical_properties=pyimport("umansysprop.critical_properties")
+liquid_densities=pyimport("umansysprop.liquid_densities")
+partition_models=pyimport("umansysprop.partition_models")
+groups=pyimport("umansysprop.groups")
+aiomfac=pyimport("umansysprop.activity_coefficient_models_dev")
+forms=pyimport("umansysprop.forms") #need forms.CoreAbundanceField (class)
 CoreAbundanceField=forms.CoreAbundanceField
-@pyimport pybel
+pybel=pyimport("pybel")
 
 function readSMILESdict()
     species2SMILESdict=Dict{String,String}()
