1da31807ecad90479f970ccd1c708f6c502b56ad
Langwen Huang <huanglangwen@outlook.com>
2019-08-11 22:00:15 +0800

    adjust building config

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,7 @@
 using Conda
 using BinaryProvider
 
-Conda.add("openbabel",channel="conda-forge")
+Conda.add("openbabel")#,channel="conda-forge")
 Conda.add("flask")
 Conda.add("flask-wtf")
 Conda.add("xlsxwriter")
@@ -13,4 +13,4 @@ products = BinaryProvider.Product[
 ]
 url = "https://github.com/huanglangwen/UManSysProp_public/archive/1.02.tar.gz"
 tarball_hash = "0cd723e69f770a5870ba198eb97a83617a702bb8c3dc010b16b59c1d4663698f"
-BinaryProvider.install(url, tarball_hash, prefix=prefix, ignore_platform=true, force=true, verbose=verbose)
+BinaryProvider.install(url, tarball_hash, prefix=prefix, ignore_platform=true, force=true, verbose=false)
