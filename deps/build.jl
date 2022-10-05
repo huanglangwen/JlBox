7e429b6fb802e1c0ec3f1f54df6cfb517f428e93
huanglangwen <huanglangwen@outlook.com>
2022-10-05 16:12:22 +0200

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -20,10 +20,11 @@ Pkg.build("PyCall")
 using Conda
 #Conda.exists failed anyway
 #if Conda.exists("openbabel==2.4.1")#Conda.exists("openbabel") doesn't work now!
-    Conda.add("openbabel",channel="conda-forge")
+#    Conda.add("openbabel",channel="conda-forge")
 #end
 root = Conda.ROOTENV
 condaPath = Sys.iswindows() ? joinpath(root, "Scripts", "conda.exe") : joinpath(root, "bin", "conda")
+run(`$(condaPath) install -y -c conda-forge openbabel=3.0.0`)
 run(`$(condaPath) install -y Werkzeug=0.16.0`)
 Conda.add("flask")
 Conda.add("flask-wtf")
